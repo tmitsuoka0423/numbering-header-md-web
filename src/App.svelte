43ea83aa6d7edccc00423941ef3349590207474e
tmitsuoka0423 <tmitsuoka0423@gmail.com>
2021-07-22 10:09:59 +0900

    add

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,62 @@
 <script>
-	export let name;
+  import sample from './sample.js';
+
+	let input = sample;
+	let result = '';
+
+	function numbering() {
+    let countHeading1 = 0;
+    let countHeading2 = 0;
+    let countHeading3 = 0;
+    let countHeading4 = 0;
+    let countHeading5 = 0;
+  
+    result = input.split('\n').map((line) => {
+      if (line.startsWith('# ')) {
+        countHeading2 = 0;
+        return `${line.substring(0, 1)} ${++countHeading1}. ${line.substring(2)}`;
+      } else if (line.startsWith('## ')) {
+        countHeading3 = 0;
+        return `${line.substring(0, 2)} ${countHeading1}-${++countHeading2}. ${line.substring(3)}`;
+      } else if (line.startsWith('### ')) {
+        countHeading4 = 0;
+        return `${line.substring(0, 3)} ${countHeading1}-${countHeading2}-${++countHeading3}. ${line.substring(4)}`;
+      } else if (line.startsWith('#### ')) {
+        countHeading5 = 0
+        return `${line.substring(0, 4)} ${countHeading1}-${countHeading2}-${countHeading3}-${++countHeading4}. ${line.substring(5)}`;
+      } else if (line.startsWith('##### ')) {
+        return `${line.substring(0, 5)} ${countHeading1}-${countHeading2}-${countHeading3}-${countHeading4}-${++countHeading5}. ${line.substring(6)}`;
+      } else {
+        return line;
+      }
+    }).join('\n');
+
+		console.log(result);
+	}
+	numbering();
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+<main class="grid">
+	<textarea on:keyup={numbering} on:blur={numbering} bind:value={input}></textarea>
+	<textarea class="result">{result}</textarea>
 </main>
 
 <style>
 	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
+		width: 100vw;
+		height: 100vh;
 	}
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
+	main.grid {
+		display: grid;
+		grid-template-columns: 0 1fr 0 1fr;
+	}
+	textarea {
+		display: block;
+		padding: 8px;
 	}
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
+	textarea.result {
+		background-color: #eeeeee;
 	}
 </style>
---
+++ src/sample.js
@@ -0,0 +1,21 @@
+export default 
+`# Numbering header markdown
+
+## How to use
+
+Input some text written by markdown at the left column.
+Then, numbered heading appears at the right side.
+
+## Heading Example
+
+We support to number heading upto level 5.
+
+## Heading2
+
+### Heading3
+
+#### Heading4
+
+##### Heading5
+
+###### Heading6`;
